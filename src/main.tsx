527c214b7d693a8acae594f4e1d56e20f14d6855
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-05-15 17:32:15 +0900

    feat: fetch mocking 기능 구현 (로그인 후 권한 획득)

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,13 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { worker } from "./mocks/browser.ts";
+
+if (process.env.NODE_ENV === 'development') {
+  worker.start()
+}
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
