685db8d0a6ea4b077861230bc5ddf6743f0efcd0
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-05-15 22:46:34 +0900

    fix: 배포 오류 수정

--- src/main.tsx
+++ src/main.tsx
@@ -5,14 +5,19 @@ import App from './App.tsx'
 import './index.css'
 import { worker } from "./mocks/browser.ts";
 
-await worker.start({
-  serviceWorker: {
-    url: '/my-msw-app/mockServiceWorker.js',
-  },
-})
+async function main() {
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+  await worker.start({
+    serviceWorker: {
+      url: '/my-msw-app/mockServiceWorker.js',
+    },
+  })
+
+  ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  )
+}
+
+main();
