354e244ad01eedddfbf9ce3af1594fc2b721c369
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-05-15 22:18:08 +0900

    fix: 배포 모드에서도 msw가 동작하도록 임시 수정

--- src/main.tsx
+++ src/main.tsx
@@ -5,9 +5,11 @@ import App from './App.tsx'
 import './index.css'
 import { worker } from "./mocks/browser.ts";
 
-if (process.env.NODE_ENV === 'development') {
-  worker.start()
-}
+// if (process.env.NODE_ENV === 'development') {
+//   worker.start();
+// }
+
+worker.start();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
